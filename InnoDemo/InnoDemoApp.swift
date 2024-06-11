04410a9f0f1fe18680e722cfcb14867543c6ba1d
Single <libobjc@gmail.com>
2024-06-11 23:27:59 +0800

    Initial Commit

---
+++ InnoDemo/InnoDemoApp.swift
@@ -0,0 +1,21 @@
+//
+//  InnoDemoApp.swift
+//  InnoDemo
+//
+//  Created by Gary on 2024/6/11.
+//
+
+import SwiftUI
+
+@main
+struct InnoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }.immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
