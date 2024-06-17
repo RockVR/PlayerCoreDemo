feaa22d54ee87de0ce52da6962db5540cd4d4b60
Single <libobjc@gmail.com>
2024-06-17 20:58:03 +0800

    Update ImmersiveView.

--- InnoDemo/ImmersiveView.swift
+++ InnoDemo/ImmersiveView.swift
@@ -14,7 +14,7 @@ struct ImmersiveView: View {
     
     var body: some View {
         RealityView { content in
-            // Init TextureResource
+            
             let img = UIImage.dummyImage(width: 1, height: 1).cgImage
             do {
                 appModel.textureResource = try .generate(from: img!, withName: "input", options: .init(semantic: .color))
@@ -27,24 +27,15 @@ struct ImmersiveView: View {
                     print(root.children)
                     if let mesh = root.findEntity(named: "Cube") as? ModelEntity {
                         
-                        let width: Float = 1.6
-                        let height: Float = 0.9
-                        let depth: Float = 0.1
+                        let width: Float = 0.64
+                        let height: Float = 0.36
                         
-                        let rectangularMesh = MeshResource.generateBox(size: [width, height, depth])
+                        let rectangularMesh = MeshResource.generatePlane(width: width, height: height)
                         mesh.model = ModelComponent(mesh: rectangularMesh, materials: mesh.model!.materials)
-                        
-//                        mesh.position = [0, 0, -1]
                         
                         if var mat = mesh.model?.materials.first as? ShaderGraphMaterial {
                             do {
-                                let uvTransform = simd_float4x4(float4(-1, 0, 0, 0),
-                                                                float4(0, 1, 0, 0),
-                                                                float4(0, 0, 1, 0),
-                                                                float4(0, 0, 0, 1))
-                                
                                 try mat.setParameter(name: "monoInput", value: .textureResource(appModel.textureResource!))
-                                try mat.setParameter(name: "monoInput_UVTransform", value: .float4x4(uvTransform))
                             } catch {
                                 print(error)
                             }
@@ -55,10 +46,8 @@ struct ImmersiveView: View {
                             
                             mesh.model?.materials = [mat]
                         }
-                        
                         content.add(mesh)
                     }
-                    
                 }
             }
         }
